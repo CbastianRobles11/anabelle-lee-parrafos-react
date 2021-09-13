44d609fc7bdaef6374015fc817b51a820eee2c37
CbastianRobles11 <cbas.secoli@gmail.com>
2021-09-12 20:21:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , Component} from 'react';
+import data from './data';
+
 
 function App() {
+
+  //el valor inicial
+  const [count,setCount]= useState(0);
+
+  //inicialice el parrafo vacio
+  const[text,setText]=useState([])
+
+
+  //funcion del formulario al hacer submit
+  const  handleSubmit=(e)=>{
+    e.preventDefault();
+
+    //lo que pasamos del index
+    console.log(count);
+    let valorIndex=count;
+
+    //paso la data el slice trae elementos de un array de tal a tal numero
+    if (valorIndex>= data.length) {
+      return  setText(data.slice(0,data.length))
+
+    }
+
+    setText(data.slice(0,valorIndex))
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>Annabel Lee</h3>
+      <h4> -Edgar Allan Poe- </h4>
+     
+     {/* //formulario donde mandaremos el numero de filas */}
+      <form className="lorem-form" onSubmit={handleSubmit}>
+          <label htmlFor="amount"> 
+            Parrafos :
+          </label>
+          <input type="number" name="amount" id="amount" 
+          value={count} 
+          //para que solo acepte numeros positivos y maximo dos
+          min="0"
+          max={data.length}
+          onChange={(e)=>{
+            //para tomar el valor de el input el setAcount
+              setCount(e.target.value)
+          }}
+          />
+
+          <button type="submit" className="btn" >Generar</button>
+
+
+      </form>
+
+      <article className="lorem-text">
+          {text.map((item,index)=>{
+            return <p key={index}>
+                {item}
+            </p>
+          })}
+          
+      </article>  
+
+    </section>
   );
 }
 
